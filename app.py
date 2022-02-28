559748705a3c192bfa6fd45f48ab39b31284624a
Pikxl <skywalker0823@hotmail.com>
2022-02-28 19:05:19 +0800

    Main files for API and data

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from crypt import methods
+from unicodedata import category
+from dotenv import load_dotenv
 from flask import *
+import pymysql
+import os
+load_dotenv()
+connection=pymysql.connect(charset='utf8',db='website',host='127.0.0.1',password=os.getenv("DB_PASS"),port=3306,user='root')
+
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+app.secret_key=os.getenv("SECRET_KEY")
+
 
 # Pages
 @app.route("/")
@@ -17,4 +28,35 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(port=3000)
+
+@app.route("/api/attractions", methods=["GET"])
+def api_attr():
+	page=request.args.get("page")
+	ender=int(page)+11
+	# keyword=request.args.get("keyword")
+	with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+		got=cursor.execute("""SELECT id,name,category2,description,address,transport,mrt,latitude,longitude,images FROM sites WHERE id>=%s AND id<=%s""",(page,ender))
+		result=cursor.fetchall()
+		connection.commit()
+		if got != 0:
+			summary=[]
+			for site in result:
+				sets={"nextPage":int(page)+1,"data":[{"id":site["id"],"name":site["name"],"category":site["category2"],"description":site["description"],"address":site["address"],"transport":site["transport"],"mrt":site["mrt"],"latitude":site["latitude"],"longitude":site["longitude"],"images":site["images"]}]}
+				summary.append(sets)
+			return jsonify(summary)
+		return jsonify({"error":True,"message":"查無資料"})
+
+@app.route("/api/attraction/<attractionId>", methods=["GET"])
+def api_atid(attractionId):
+	with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+		got=cursor.execute("""SELECT id,name,category2,description,address,transport,mrt,latitude,longitude,images FROM sites WHERE id=%s""",(attractionId))
+		site=cursor.fetchone()
+		connection.commit()
+		if got!=0:
+			summary={"data":{"id":site["id"],"name":site["name"],"category":site["category2"],"description":site["description"],"address":site["address"],"transport":site["transport"],"mrt":site["mrt"],"latitude":site["latitude"],"longitude":site["longitude"],"images":site["images"]}}
+			return jsonify(summary)
+		return jsonify({"error":True,"message":"查無資料"})
+
+
+if __name__=="__main__":
+	app.run(debug=True, port=3000)
---
+++ data/data_analysis.py
@@ -0,0 +1,73 @@
+import json
+import pymysql
+import os
+from dotenv import load_dotenv
+datas=open("taipei-attractions.json")
+datas_json=json.load(datas)
+
+#創立table
+def build(conn):
+    cursor.execute("DROP TABLE IF EXISTS sites")
+    sql = """CREATE TABLE sites(
+        id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name varchar(255) NOT NULL,
+        category varchar(255),
+        category2 varchar(255),
+        description TEXT,
+        address varchar(255),
+        transport TEXT,
+        mrt varchar(255),
+        latitude varchar(255),
+        longitude varchar(255),
+        images TEXT
+    )"""
+    cursor.execute(sql)
+    conn.commit()
+
+
+#輸入資料
+def insert_data(conn):
+            with conn.cursor() as cursor:
+                sites=datas_json["result"]["results"]
+                images=[]
+                for site in sites:
+                    pics=site["file"].lower().split("jpg")
+                    name=site["stitle"]
+                    cat1=site["CAT1"]
+                    cat2=site["CAT2"]
+                    des=site["xbody"]
+                    add=site["address"]
+                    trans=site["info"]
+                    mrt=site["MRT"]
+                    lati=site["latitude"]
+                    longi=site["longitude"]
+                    for pic in pics:
+                        if pic.endswith("."):
+                            img=pic+"jpg"
+                            images.append(img)
+                    result=cursor.execute(
+                        """INSERT INTO
+                            sites(
+                                name,
+                                category,
+                                category2,
+                                description,
+                                address,
+                                transport,
+                                mrt,
+                                latitude,
+                                longitude,
+                                images
+                                )
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",(name,cat1,cat2,des,add,trans,mrt,lati,longi,str(images)))
+                    conn.commit()
+                    print("完成 新增: ",result,"筆資料")
+
+if __name__=="__main__":
+    load_dotenv()
+    conn=pymysql.connect(charset='utf8',db='website',host='127.0.0.1',password=os.getenv("DB_PASS"),port=3306,user='root')
+    cursor=conn.cursor()
+    build(conn)
+    insert_data(conn)
+    cursor.close()
+    datas.close()
