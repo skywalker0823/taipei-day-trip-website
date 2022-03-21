d89df6649f17da3fc4efebfe611f7e1c72adf5cd
Pikxl <skywalker0823@hotmail.com>
2022-03-21 15:51:30 +0800

    fix hover hand and path details

--- member_manage.py
+++ member_manage.py
@@ -9,6 +9,7 @@ import os
 import sys, traceback
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import set_access_cookies
+
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import unset_jwt_cookies
 from flask_jwt_extended import get_jwt_identity
