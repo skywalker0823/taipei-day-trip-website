4e6e3e9fd8b3e3a075bf312309ad8d4a45e65990
Pikxl <skywalker0823@hotmail.com>
2022-03-28 17:50:08 +0800

    database adjustment and pooled

--- app.py
+++ app.py
@@ -17,8 +17,30 @@ from flask_jwt_extended import JWTManager
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
-# load_dotenv()
-connection=pymysql.connect(charset='utf8',db='website',host='127.0.0.1',password="",port=3306,user='root')
+from dbutils.pooled_db import PooledDB
+
+
+
+POOL = PooledDB(
+    creator=pymysql,  # Which DB module to use
+    maxconnections=6,  # Allowed max connection, 0 and None means no limitations.
+    mincached=2,  # Least connection when created, 0 means don't.
+    blocking=True,  # Queue when there is no connection avaliable. True = wait；False = No waits, and report error.
+    ping=0, # Check if Mysql service is avaliable # if：0 = None = never, 1 = default = whenever it is requested, 2 = when a cursor is created, 4 = when a query is executed, 7 = always
+
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    password="",
+    database='website',
+    charset='utf8',
+    cursorclass=pymysql.cursors.DictCursor
+)
+connection = POOL.connection()
+
+
+
+# connection=pymysql.connect(charset='utf8',db='website',host='127.0.0.1',password="",port=3306,user='root')
 
 
 app=Flask(__name__,
@@ -164,4 +186,4 @@ def my_expired_token_callback(jwt_header, jwt_payload):
 
 
 if __name__=="__main__":
-	app.run(host='0.0.0.0',port=3000)
+	app.run(host='0.0.0.0',port=3000,debug=True)
